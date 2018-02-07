e0771ad8b5a67c014700c00720d0b1af3887894d
Ben Angek <ben@Bens-MacBook-Air.local>
2018-02-07 10:35:39 -0700

    test 9 done

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,6 +1,14 @@
 // Write a JavaScript function removeDuplicates that removes duplicate values fom an array.
 
-var removeDuplicates;
+function removeDuplicates(){
+  var newAray = []
+  for (var i = 0; i < arr.length; i++){
+    if (newAray.indexOf(arr[i]) == -1) {
+      newArray.push(arr[i])
+    }
+  }
+  return newAray
+}
 
 // DO NOT DELETE
 module.exports = removeDuplicates;
